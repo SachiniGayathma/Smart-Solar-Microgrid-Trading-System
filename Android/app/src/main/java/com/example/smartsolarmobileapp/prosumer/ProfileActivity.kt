735051cfd5de2d61ec25014c1977b8f9d61a87c5
YenuliAmaratunga <yenulibeemansi@gmail.com>
2026-09-25 17:27:51 +0530

    feat(prosumer): initialize prosumer folder structure, activities, and layouts

--- Android/app/src/main/java/com/example/smartsolarmobileapp/prosumer/ProfileActivity.kt
+++ Android/app/src/main/java/com/example/smartsolarmobileapp/prosumer/ProfileActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.smartsolarmobileapp.R
 
-class QRDisplayActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_qr_display)
+        setContentView(R.layout.activity_profile)
     }
 }
---
+++ Android/app/src/main/java/com/example/smartsolarmobileapp/prosumer/adapter/BookingAdapter.kt
@@ -0,0 +1,5 @@
+package com.example.smartsolarmobileapp.prosumer.adapter
+
+class BookingAdapter {
+    // RecyclerView Adapter for reservations and booking history
+}
